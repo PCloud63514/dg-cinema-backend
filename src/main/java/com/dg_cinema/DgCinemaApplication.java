4f4d70cb730e9e5f5b0d84866af771f289d215ce
TAE HEON KWON <PCloud63514@gmail.com>
2024-12-01 17:17:52 +0900

    close #1

---
+++ src/main/java/com/dg_cinema/DgCinemaApplication.java
@@ -0,0 +1,12 @@
+package com.dg_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgCinemaApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(DgCinemaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dg_cinema/DgCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
